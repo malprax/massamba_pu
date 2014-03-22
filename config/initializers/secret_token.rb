5672532e8974eaaaa11d29110299a5589e69d22a
aulia <kingmalprax@gmail.com>
2014-03-23 07:39:50 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MassambaPu::Application.config.secret_key_base = '42b6cedcc0061f697ab026fb2a2e2717b1fb7497c45b187e1684aee529173d6657e21d682c3a246d7d99c276904eb327101c4cd18f0c5e722d136e317637cb6a'
