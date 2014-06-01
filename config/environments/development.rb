5f5ef42ce82273c27a4ee1a4cfdfa21c1588973d
aulia <kingmalprax@gmail.com>
2014-06-02 06:47:26 +0800

    add confirmation email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,12 +17,27 @@ MassambaPu::Application.configure do
   config.action_mailer.raise_delivery_errors = false
   
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
   # Raise an error on page load if there are pending migrations
   config.active_record.migration_error = :page_load
+  
+  #sending email   
+   config.action_mailer.perform_deliveries = true
+   config.action_mailer.default :charset => "utf-8"
+   config.action_mailer.smtp_settings = {
+     address: "smtp.gmail.com",
+     port: 587,
+     domain: "localhost",
+     authentication: :login,
+     enable_starttls_auto: true,
+     user_name: 'kingmalprax@gmail.com',
+     password: '@1603auRIS'
+   }
 
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
