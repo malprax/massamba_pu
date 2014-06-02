0930cf44f5a4923976a455a08df8e90fb7d47615
aulia <kingmalprax@gmail.com>
2014-06-03 06:10:58 +0800

    edit confirmation email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,9 +14,8 @@ MassambaPu::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-  
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.raise_delivery_errors = false  
+  config.action_mailer.default_url_options = { host: 'massamba_pu.dev' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
 
@@ -32,7 +31,7 @@ MassambaPu::Application.configure do
    config.action_mailer.smtp_settings = {
      address: "smtp.gmail.com",
      port: 587,
-     domain: "localhost",
+     domain: "massamba_pu.dev",
      authentication: :login,
      enable_starttls_auto: true,
      user_name: 'kingmalprax@gmail.com',
