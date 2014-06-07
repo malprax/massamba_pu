a3adcbe25129c15c9346295924a8f51b7bf6c734
aulia <kingmalprax@gmail.com>
2014-06-07 23:11:46 +0800

    add timeoutable in devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :timeoutable, :timeout_in => 30.minutes
          
    # attr_accessible :password, :password_confirmation, :remember_me, :email
    validates_presence_of :email, :password
