8d5f8741c2c32e5e991113242c6b8180531ed22a
aulia <kingmalprax@gmail.com>
2014-06-09 11:44:31 +0800

    try send email6

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ MassambaPu::Application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "gmail.com",
+    domain: "heroku.com",
     authentication: :login,
     enable_starttls_auto: true,
     user_name: 'kingmalprax@gmail.com',
