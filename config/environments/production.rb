41a225ac199a09993741d95eea7d12b870107be0
aulia <kingmalprax@gmail.com>
2014-06-09 19:04:37 +0800

    config dalli in production and initializer/session_store

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,6 +52,7 @@ MassambaPu::Application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production.
+    config.cache_store = :dalli_store
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
-
-MassambaPu::Application.config.session_store :cookie_store, key: '_massamba_pu_session'
+#cookie_store, key: '_massamba_pu_session'
+MassambaPu::Application.config.session_store ActionDispatch::Session::CacheStore, :expire_after => 30.minutes
