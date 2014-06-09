907381ddc3254759aea315b37df8065062a4abbf
aulia <kingmalprax@gmail.com>
2014-06-09 11:38:12 +0800

    try send email5

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,14 +80,14 @@ MassambaPu::Application.configure do
   
   
   #send_confirmation_email
-  config.action_mailer.default_url_options = {:host => 'dpulutra.com'}
+  config.action_mailer.default_url_options = {:host => 'www.dpulutra.com'}
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "heroku.com",
+    domain: "gmail.com",
     authentication: :login,
     enable_starttls_auto: true,
     user_name: 'kingmalprax@gmail.com',
