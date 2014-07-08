a296b7d47f5c23d70563e4f9996478213d708d62
aulia <kingmalprax@gmail.com>
2014-07-08 19:15:06 +0800

    edit album controller

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,39 +1,34 @@
 class AlbumsController < ApplicationController
-  # before_action :set_album, only: [:show, :edit, :update, :destroy]
-  # before_action :set_galery, only: [:index, :show, :new, :edit, :create, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_album, only: [:show, :edit, :update, :destroy]
+  before_action :set_galery, only: [:index, :show, :new, :edit, :create, :update, :destroy]
 
   # GET /albums
   # GET /albums.json
-  def index
-    @galery = Galery.find(params[:galery_id])
+  def index   
     @albums = @galery.albums
   end
 
   # GET /albums/1
   # GET /albums/1.json
-  def show 
-    @galery = Galery.find(params[:galery_id])    
-    @album = @galery.albums.find(params[:id])
+  def show    
+   
   end
 
   # GET /albums/new
-  def new  
-    @galery = Galery.find(params[:galery_id])   
+  def new      
     @album = @galery.albums.build
   end
 
   # GET /albums/1/edit
-  def edit   
-    @galery = Galery.find(params[:galery_id]) 
-    @album = @galery.albums.find(params[:id])  
+  def edit      
+    
   end
 
   # POST /albums
   # POST /albums.json
-  def create
-    @galery = Galery.find(params[:galery_id])     
+  def create   
     @album = @galery.albums.create(album_params)
-
     respond_to do |format|
       if @album.save
         format.html { redirect_to galery_albums_path, notice: 'Album berhasil dibuat.' }
@@ -48,9 +43,8 @@ class AlbumsController < ApplicationController
 
   # PATCH/PUT /albums/1
   # PATCH/PUT /albums/1.json
-  def update 
-    @galery = Galery.find(params[:galery_id])      
-    @album = @galery.albums.find(params[:id])      
+  def update     
+       
     respond_to do |format|
       if @album.update(album_params)
         format.html { redirect_to [@album.galery, @album], notice: 'Album berhasil di update.' }
@@ -65,9 +59,7 @@ class AlbumsController < ApplicationController
 
   # DELETE /albums/1
   # DELETE /albums/1.json
-  def destroy 
-    @galery = Galery.find(params[:galery_id])
-    @album = @galery.albums.find(params[:id])            
+  def destroy                   
     @album.destroy
     respond_to do |format|
       format.html { redirect_to @album.galery }
@@ -77,12 +69,12 @@ class AlbumsController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    # def set_galery
-#         @galery = Galery.find(params[:galery_id])
-#     end
-    # def set_album
- #       @album = Album.find(params[:id])
- #    end
+  def set_galery
+      @galery = Galery.find(params[:galery_id])
+  end
+  def set_album
+      @album = @galery.albums.find(params[:id])
+  end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def album_params
