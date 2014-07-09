5d38f87a10c261188dcfbfacfaff16a1be8a1510
aulia <kingmalprax@gmail.com>
2014-07-09 11:19:51 +0800

    add file to dropbox

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,15 +1,13 @@
 class Album < ActiveRecord::Base
   belongs_to :galery 
   has_attached_file  :image,
- #                     :storage => :dropbox, 
- #                     :dropbox_credentials => "#{Rails.root}/config/dropbox.yml",
- #                     dropbox_visibility: 'public',
-                      styles: { :medium => "300x300>"} 
- #                     :default_url => "/images/:style/missing.png",
-                     # :url => "/assets/images/:id/:style/:basename.:extension",
-                     # :dropbox_options => {
- #                           :path => proc { |style| "#{style}/#{id}_#{image.original_filename}" }
- #                         }
-      #               :path => ":rails_root/public/assets/images/:id/:style/:basename.:extension"                
-      validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/ 
+                     :storage => :dropbox,
+                     :dropbox_credentials => "#{Rails.root}/config/dropbox.yml",                    
+                      styles: { :medium => "300x300>"},
+                     :default_url => "/images/:style/missing.png",                     
+                     :dropbox_options => {
+                           :path => proc { |style| "#{style}/#{id}_#{image.original_filename}" }
+                         }                    
+  
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/ 
 end
